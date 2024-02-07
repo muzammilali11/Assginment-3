aafb70bbaa5e5d59739abeefb9f38ecec9bc7d96
Muzammil Ali <153710574+muzammilali11@users.noreply.github.com>
2024-02-07 11:19:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,121 @@
+// //question no 1
+// var city = prompt("The city of lights ?");
+// if (city === "karachi") {
+//   alert("Welcome to city of lights");
+// }
+// //question no 2
+// var gender = prompt("Please enter your gender").toLowerCase();
+// if(gender===male){
+//     alert=("Good moring sir")
+// }
+// if(gender==="Female"){
+//     alert=("Good moring mam")
+// }
+//question no 3
+// var littres = prompt("remaining fuel in car");
+// if (littres <= 0.25) {
+//   console.log("Please refill the fuel in your car");
+// }
+// if (littres >= 0.25) {
+//   console.log("Good");
+// }
+// question no 4
+// a.
+
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable b is true");
+// }
+//         "This condition for variable a is true"
+
+// b.
+
+// var b = 82;
+// if (b++ === 83) {
+//     alert("given condition for variable b is true");
+// }
+//         "This condition for variable b is not true"
+
+// c.
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+//         "This condition 1,3 is not true"
+//         "This condition 2,4 is true"
+
+// d.
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//            "This condition is true"
+
+// e.
+
+//  if (true){
+//  alert("True");
+// } if (false){
+// alert("False");
+// }
+//             "This condition is true"
+
+// f.
+
+//  if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+//            "This condition is true"
+//question no 5
+// var secretNumber = +prompt("Guess your number from 1 to 10 ");
+// var secret = 6;
+// var guess = 5;
+// if (secretnumber === secret) {
+//   console.log("Bingo!correct");
+// }
+// else if(secretNumber===guess){
+//     console.log('close enough to the Correct answer')
+// }
+//question no 6
+// var num = +prompt("enter the divisible number");
+// if(num%3===0){
+//     console.log("it is divisible")
+//      }else{
+//          console.log("Not divisbile")
+//      }
+//question no 7
+// var num =+ prompt('Enter the number ')
+// if(num%2===0){
+//     console.log('it is a Even number')
+// }else{
+//     console.log('it is a Odd number')
+// }
+
+
+// Question.8
+// var T = prompt ('What is temperature today')           
+// if (T >= 40){
+//     console.log('It is too hot outside')
+// }
+// else if (T >= 30)[
+//     console.log('The Weather today is Normal')
+// ]
+// else if (T >= 20)[
+//     console.log('Today’s Weather is cool')
+// ]
+// else if (T >= 10)[
+//     console.log('OMG! Today’s weather is so Cool')
+// ]
